4337f2da85f908bd24721e567f7961e62d2fdcf5
zy-yao <33860709+zy-yao@users.noreply.github.com>
2020-06-24 20:12:47 +0800

    Create DBOpenHelper.java

---
+++ Edge_Computing/codes/app/src/main/java/com/cquptkiller/a_guarder/utils/DBOpenHelper.java
@@ -0,0 +1,26 @@
+package com.cquptkiller.a_guarder.utils;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by yao on 2018/12/22.
+ */
+public class DBOpenHelper extends SQLiteOpenHelper {
+
+    public DBOpenHelper(Context context) {
+        super(context, "upload.db", null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE uploadlog (_id integer primary key autoincrement, uploadfilepath varchar(100), sourceid varchar(10))");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS uploadlog");
+        onCreate(db);
+    }
+
+}
